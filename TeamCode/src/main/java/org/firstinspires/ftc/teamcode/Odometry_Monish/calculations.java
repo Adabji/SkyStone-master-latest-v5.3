d9b0815e55b76ab78b5854ee433b859709ba60a8
Monish Reddy Kotturu <51919815+Monish-Reddy-Kotturu@users.noreply.github.com>
2020-08-04 17:35:00 -0400

    LinearOpMode test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry_Monish/calculations.java
@@ -0,0 +1,98 @@
+package org.firstinspires.ftc.teamcode.Odometry_Monish;
+
+import com.qualcomm.robotcore.util.Range;
+
+import java.util.Arrays;
+
+import static org.firstinspires.ftc.teamcode.Odometry_Monish.mainDriver.globalXPosEncoderTicks;
+import static org.firstinspires.ftc.teamcode.Odometry_Monish.mainDriver.globalYPosEncoderTicks;
+import static org.firstinspires.ftc.teamcode.Odometry_Monish.mainDriver.heading;
+
+public class calculations {
+    public static double Theta, c, linearDistance, rotationalDistance;
+    public static double outputX;
+    public static double outputY, headingForTurning;
+    public static double reductionDistance;
+    public static double distanceToTurn;
+    public static double leftFrontPower, rightFrontPower, leftBackPower, rightBackPower, powerReduction;
+    public static double circumference = 64.42;
+
+    //The amount of encoder ticks for each inch the robot moves. This will change for each robot and needs to be changed here
+    final static double COUNTS_PER_INCH = 1141.94659527;
+
+    //Hardware map names for the encoder wheels. Again, these will change for each robot and need to be updated below
+    public static double globalXPos, globalYPos, proportionPowerReduction, turnPower;
+    public static double xPowerRatio, yPowerRatio;
+
+    public static double previousError, previousTime, currentTime, p, i, d, pidOutput;
+
+    public static double pidCalculations(double currentError){
+
+        currentTime = System.currentTimeMillis();
+
+        p = currentError / 18;
+        d = ((currentError - previousError) / (currentTime - previousTime)) / 100;
+        i = 0;
+
+        pidOutput = Range.clip((p + i + d), 0, 1);
+
+        previousError = currentError;
+        previousTime = currentTime;
+
+        return pidOutput;
+    }
+
+    public static double[] goToPositionCalculations (double desiredXCoordinate, double desiredYCoordinate, double desiredHeading) {
+        //Converting the odometry readings in encoder ticks to inches
+        globalXPos = globalXPosEncoderTicks / COUNTS_PER_INCH;
+        globalYPos = globalYPosEncoderTicks / COUNTS_PER_INCH;
+
+        //Getting the ratio of motor powers based off the distance to target in each axis
+        xPowerRatio = (desiredXCoordinate - globalXPos);
+        yPowerRatio = (desiredYCoordinate - globalYPos);
+
+        //Finding the reduction factor based off the distance to target
+        reductionDistance = Range.clip(c, 0, 25);
+        proportionPowerReduction = Range.clip(Math.sqrt(Math.abs(c / 25)), 0, 1);
+
+        if (heading >= 0) {
+            headingForTurning = heading;
+        }
+        if (heading < 0) {
+            headingForTurning = heading + 360;
+        }
+
+        distanceToTurn = desiredHeading - Math.toDegrees(heading);
+        turnPower = distanceToTurn / 360 * circumference;
+        return driveMecanum(xPowerRatio, yPowerRatio, turnPower, 0);
+    }
+
+    public static double[] driveMecanum(double xPower, double yPower, double turnPower, double reduction) {
+        rotationalDistance = Math.abs((distanceToTurn/360)*circumference);
+        linearDistance = Math.sqrt(xPower * xPower + yPower * yPower);
+        c = linearDistance+ Math.abs((distanceToTurn/360)*circumference);
+
+        Theta = Math.atan2(xPower, yPower);
+
+        outputY = Math.cos(heading - Theta)*linearDistance;
+        outputX = -Math.sin(heading - Theta)*linearDistance;
+
+        leftFrontPower = (outputY + outputX) + turnPower;
+        leftBackPower = (outputY - outputX) + turnPower;
+        rightFrontPower = (outputY - outputX) - turnPower;
+        rightBackPower = (outputY + outputX) - turnPower;
+
+        powerReduction = reduction;
+
+        double[] wheelPowers = {Math.abs(rightFrontPower), Math.abs(leftFrontPower), Math.abs(leftBackPower), Math.abs(rightBackPower)};
+        Arrays.sort(wheelPowers);
+        double biggestInput = wheelPowers[3];
+
+        leftFrontPower /= biggestInput;
+        leftBackPower /= biggestInput;
+        rightFrontPower /= biggestInput;
+        rightBackPower /= biggestInput;
+
+        return new double[]{leftFrontPower, leftBackPower, rightFrontPower, rightBackPower, c};
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry_Monish/mainDriver.java
@@ -0,0 +1,109 @@
+package org.firstinspires.ftc.teamcode.Odometry_Monish;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.Odometry.OdometryGlobalCoordinatePosition;
+
+import org.firstinspires.ftc.teamcode.Odometry_Monish.calculations;
+
+@Config
+@Autonomous(name = "OdometryGoToPosition", group = "Autonomous")
+public class mainDriver extends LinearOpMode {
+    private static DcMotor leftFrontWheel, leftBackWheel, rightFrontWheel, rightBackWheel;
+
+    //Odometry encoder wheels
+    DcMotor verticalRight, verticalLeft, horizontal;
+
+    //Hardware map names for the encoder wheels. Again, these will change for each robot and need to be updated below
+    String verticalLeftEncoderName = "intake motor 2", verticalRightEncoderName = "intake motor 1", horizontalEncoderName = "intake motor 3";
+
+    OdometryGlobalCoordinatePosition globalPositionUpdate;
+    Thread positionThread;
+
+    final double COUNTS_PER_INCH = 1141.94659527;
+    public static double heading, globalXPosEncoderTicks, globalYPosEncoderTicks;
+
+    public void runOpMode() {
+        while (!opModeIsActive() && !isStopRequested()) {
+            // goToPosition = new MotorPowerMecanum();
+            // pid = new PIDCalulations();
+
+            leftFrontWheel = hardwareMap.dcMotor.get("left front");
+            leftBackWheel = hardwareMap.dcMotor.get("left back");
+            rightFrontWheel = hardwareMap.dcMotor.get("right front");
+            rightBackWheel = hardwareMap.dcMotor.get("right back");
+            verticalLeft = hardwareMap.dcMotor.get(verticalLeftEncoderName);
+            verticalRight = hardwareMap.dcMotor.get(verticalRightEncoderName);
+            horizontal = hardwareMap.dcMotor.get(horizontalEncoderName);
+
+            //Reset the encoders
+            verticalRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            verticalLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            horizontal.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            globalPositionUpdate = new OdometryGlobalCoordinatePosition(verticalLeft, verticalRight, horizontal, COUNTS_PER_INCH, 75);
+            positionThread = new Thread(globalPositionUpdate);
+            positionThread.start();
+
+            globalPositionUpdate.reverseLeftEncoder();
+
+            /*
+            Reverse the direction of the odometry wheels. THIS WILL CHANGE FOR EACH ROBOT. Adjust the direction (as needed) of each encoder wheel
+            such that when the verticalLeft and verticalRight encoders spin forward, they return positive values, and when the
+            horizontal encoder travels to the right, it returns positive value
+            */
+            verticalLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            verticalRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            horizontal.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            //Set the mode of the odometry encoders to RUN_WITHOUT_ENCODER
+            verticalRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            verticalLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            horizontal.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            //Init complete
+            telemetry.addData("Status", "Init Complete");
+            telemetry.update();
+
+            rightFrontWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+            rightBackWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
+
+        if (opModeIsActive()) {
+            heading = globalPositionUpdate.robotOrientationRadians;
+            globalXPosEncoderTicks = globalPositionUpdate.returnXCoordinate();
+            globalYPosEncoderTicks = globalPositionUpdate.returnYCoordinate();
+
+            double[] powers = calculations.goToPositionCalculations(25, 25, 180);
+
+            setPower(powers[0], powers[1], powers[2], powers[3], powers[4]);
+        }
+    }
+
+    /*telemetry.addData("heading", heading);
+    telemetry.addData("Theta",Theta);
+    telemetry.addData("p", p);
+    telemetry.addData("d", d);
+    telemetry.addData("headingForTurning", headingForTurning);
+    telemetry.addData("distanceToTurn", distanceToTurn);
+    telemetry.addData("leftFrontPower", leftFrontPower*pidOutput);
+    telemetry.addData("rightFrontPower", rightFrontPower*pidOutput);
+    telemetry.addData("leftBackPower", leftBackPower*pidOutput);
+    telemetry.addData("rightBackPower", rightBackPower*pidOutput);
+    telemetry.addData("xPowerRatio", xPowerRatio);
+    telemetry.addData("yPowerRatio", yPowerRatio);
+    telemetry.addData("c", c);
+    telemetry.update();*/
+
+    public static void setPower(double lf, double lb, double rf, double rb, double c) {
+        double pidOutput = calculations.pidCalculations(c);
+        leftFrontWheel.setPower(lf*pidOutput);
+        rightFrontWheel.setPower(rf*pidOutput);
+        leftBackWheel.setPower(lb*pidOutput);
+        rightBackWheel.setPower(rb*pidOutput);
+    }
+}
